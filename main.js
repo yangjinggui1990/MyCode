cee0c230f2623d93c988c776550497c13b0b7259
yangjinggui <yangjinggui_1990@163.com>
2020-05-29 10:06:36 +0800

    Code

---
+++ main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import element from "./element";
+import vant from "./vant";
+
+import "vant/lib/index.css";
+
+Vue.config.productionTip = false;
+
+Vue.use(element);
+Vue.use(vant);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
